0afc1ead70a69b5c73d8e99ad454586a05e2fc3d
Beth Ruff <beth.ruff2@gmail.com>
2020-02-26 23:19:12 +0000

    Automatically backed up by Learn

--- lib/scraper.rb
+++ lib/scraper.rb
@@ -1,9 +1,29 @@
 require 'nokogiri'
 require 'open-uri'
+require 'pry'
 
 require_relative './course.rb'
 
 class Scraper
+  
+  def get_page
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+  end 
+  
+  def get_courses 
+    doc = Nokogiri::HTML(open("http://learn-co-curriculum.github.io/site-for-scraping/courses"))
+    
+    doc.css(".post").each do |post|
+      course = Course.new 
+      course.title = post.css("h2").text 
+      course.schedule = post.css(".date").text
+      course.description = post.css("p").text 
+    end 
+    binding.pry 
+  end 
+  
+  def make_courses 
+  end 
   
   def print_courses
     self.make_courses
@@ -18,5 +38,5 @@ class Scraper
   
 end
 
-
+Scraper.new.get_page
 
